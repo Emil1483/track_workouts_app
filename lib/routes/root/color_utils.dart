c3263c9e2b891ec7330959cd9142b683dd82b22a
Emil Djupvik <emil14833@gmail.com>
2020-10-08 19:33:29 +0200

    fix workout color

--- lib/routes/root/color_utils.dart
+++ lib/routes/root/color_utils.dart
@@ -1,14 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:track_workouts/routes/root/root_viewmodel.dart';
 
 class ColorUtils {
   static Color getColorFrom(FormattedWorkout workout) {
     final List<double> hues = [];
+    final hashLength = 4;
     workout.exercises.forEach((exercise) {
       final hash = exercise.name.toLowerCase().hashCode.toString();
-      final subHash = hash.substring(hash.length - 3);
+      final subHash = hash.substring(hash.length - hashLength);
       final hashNumber = int.parse(subHash);
-      hues.add(hashNumber * 360 / 1000);
+      hues.add(hashNumber * 360 / pow(10, hashLength));
     });
     final hue = hues.reduce((a, b) => a + b) / hues.length;
 

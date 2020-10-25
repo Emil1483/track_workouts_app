0d28d10f3e97db089b0408cb70654bc85b630abc
Emil Djupvik <emil14833@gmail.com>
2020-10-25 22:11:11 +0100

    fix week number

--- lib/utils/models/week.dart
+++ lib/utils/models/week.dart
@@ -17,14 +17,11 @@ class Week {
   Week copy() => Week(_weekStart);
 
   int get weekNumber {
-    DateTime startOfYear = DateTime(_weekStart.year, 1, 1, 0, 0);
-    DateTime weekOne;
-    if (startOfYear.weekday > 4) {
-      weekOne = startOfYear.add(Duration(days: 7)).flooredToWeek;
-    } else {
-      weekOne = startOfYear.flooredToWeek;
-    }
-    return (_weekStart.difference(weekOne).inHours / (24 * 7)).round();
+    DateTime weekOne = DateTime(_weekStart.year, 1, 1, 0, 0).flooredToWeek;
+    if (weekOne.weekday > 4) weekOne = weekOne.add(Duration(days: 7));
+    
+    final difference = end.difference(weekOne).inHours;
+    return (difference / (24 * 7)).round();
   }
 
   String get weekString => 'Week $weekNumber';
